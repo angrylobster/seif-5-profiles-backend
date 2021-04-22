6805b3831a05c61cb3e773c4eaa4b0ab8f67034c
Min Shan <minshan.liew@bookmyshow.com>
2021-04-22 18:00:55 +0800

    change backend homework api

--- src/homework/homework.controller.ts
+++ src/homework/homework.controller.ts
@@ -4,30 +4,28 @@ import { SheetsService } from '../sheets/sheets.service';
 import { Homework } from './homework.dtos';
 
 @Controller('homework')
+@UseGuards(JwtGuard)
 export class HomeworkController {
     constructor (private readonly sheetsService: SheetsService) {}
-
-    @Get('columns')
-    async getHomeworkColumns (): Promise<Homework[]> {
+    @Get('')
+    async getAllByStudent (@Request() req): Promise<Homework[]> {
         const homeworkSheet = await this.sheetsService.getHomeworkSheet();
         const names = homeworkSheet.getHomeworkNames();
         const lessons = homeworkSheet.getLessons();
         const dueDates = homeworkSheet.getDueDates();
-        return lessons.map((lesson: string, index: number) => 
-            new Homework(names[index], lesson, dueDates[index]),
-        );
-    }
-
-    @Get('student')
-    @UseGuards(JwtGuard)
-    async getHomeworkByStudent (@Request() req): Promise<string[]> {
-        const homeworkSheet = await this.sheetsService.getHomeworkSheet();
         const emails = homeworkSheet.getEmails();
         const studentIndex = emails.indexOf(req.user.email);
         if (studentIndex === -1) {
             throw new NotFoundException(`Could not find student with email ${req.user.email}`);
         }
         const completionData = homeworkSheet.getCompletion(studentIndex);
-        return completionData;
+        return lessons.map((lesson: string, index: number) => {
+            return new Homework(
+                names[index],
+                lesson,
+                dueDates[index],
+                completionData[index],
+            );
+        });
     }
 }
--- src/homework/homework.dtos.ts
+++ src/homework/homework.dtos.ts
@@ -2,14 +2,17 @@ export class Homework {
     name: string;
     lesson: string;
     dueDate: string;
+    completion?: string;
 
     constructor (
         name: string,
         lesson: string,
         dueDate: string,
+        completion?: string,
     ) {
         this.name = name;
         this.lesson = lesson;
         this.dueDate = dueDate;
+        this.completion = completion;
     }
 }
